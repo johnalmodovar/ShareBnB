accab907b61fa4fa41226737447e1ca8e8f41b13
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-12-12 15:13:29 -0800

    added port access from env

--- backend-sharebnb/config.js
+++ backend-sharebnb/config.js
@@ -6,14 +6,14 @@ require("dotenv").config();
 
 const SECRET_KEY = process.env.SECRET_KEY;
 const BUCKET = "sharebnb-jm";
-const PORT = 3001;
+const PORT = +process.env.PORT || 3001;
 
 
 /** Use dev database, testing database, or via env var, production database */
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
     ? "postgresql:///sharebnb_test"
-    : process.env.DATABASE_URL || "postgresql:///sharebnb";
+    : process.env.DATABASE_URL;
 }
 
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
